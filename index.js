117c4019284d6c443211437555aeb184223eaa3d
fatima arifeen <fatima.arifeen24@gmail.com>
2024-12-04 20:36:07 +0600

    jwt token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
@@ -30,24 +30,29 @@ async function run() {
     const cartsCollection = client.db("savvoryDB").collection("carts");
 
         // jwt related api
-    //     function createToken(user){
-    //       const token = jwt.sign({
-    //         email: user.email
-    //       }, 'secret', { expiresIn: '7d' });
-    //       return token;
-    //     }
+
+        app.post('/jwt', async (req,res)=>{
+          const user = req.body;
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1h'});
+            res.send({ token });
+        })
         
-    //     function verifyToken(req,res,next){
-    //     const token = req.headers.authorization.split(" ")[1];
-    //     console.log(token);
-    //     const verify = jwt.verify(token, 'secret');
-    //     console.log(verify);
-    //     if(!verify?.email){
-    //       return res.send('you are not authorized')
-    //     }
-    //     req.user = verify.email;
-    //       next()
-    //     }
+        // middlewares
+        const verifyToken = (req, res, next) => {
+          console.log('verified token', req.headers.authorization);
+          if(!req.headers.authorization){
+            return res.status(401).send({message: 'forbidden access'});
+          }
+          const token = req.headers.authorization.split(' ')[1];
+          jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+            if(err){
+              return res.status(401).send({message: 'forbidden access'})
+            }
+            req.decoded = decoded;
+            next();
+          })
+        }
 
     // // use verify admin after verifyToken
     // const verifyAdmin = async (req, res, next) => {
@@ -92,7 +97,7 @@ async function run() {
 
   // users api
 
-  app.post('/users', async (req, res) => {
+  app.post('/users',verifyToken, async (req, res) => {
     const user = req.body;
     // insert email if user doesn't exist:
     // can be done many ways (1. email unique 2. upsert 3. simple checking)
@@ -105,49 +110,30 @@ async function run() {
     res.send(result);
   })
 
-//  app.post('/users', async(req,res)=>{
-//   const user = req.body;
-//   const token = createToken(user);
-//   console.log(token)
-//   const isUserExist = await userCollection.findOne({email: user?.email})
-//   console.log(isUserExist)
-//   if(isUserExist?._id){
-//     return res.send({
-//       status:"success",
-//       message:"login success",
-//       token
-//     })
-//   }
-//   await userCollection.insertOne(user);
-//   res.send(token)
-//  })
-
-
   app.get('/users', async (req,res)=>{
   const result = await userCollection.find().toArray();
   res.send(result);
 });
 
 
-//  app.get('/users/get/:id', async (req,res)=>{
-//   const id = req.params.id;
-//   const result = await userCollection.findOne({_id:new ObjectId(id)});
-//   res.send(result);
-//   console.log(result)
-//  })
+app.delete('/users/:id', async (req, res)=> {
+const id = req.params.id;
+const query = {_id: new ObjectId(id)}
+const result = await userCollection.deleteOne(query);
+res.send(result);
+})
 
-//  app.get('/users/:email', async (req,res)=>{
-//   const email = req.params.email;
-//   const result = await userCollection.findOne({email});
-//   res.send(result);
-//  })
-
-//  app.patch('/users/:email', async (req,res)=>{
-//   const email = req.params.email;
-//   const userData = req.body;
-//   const result = await userCollection.updateOne({email},{$set: userData},{upsert:true});
-//   res.send(result);
-//  })
+app.patch('/users/admin/:id', async (req,res)=> {
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const updatedDoc = {
+    $set: {
+      role: 'admin'
+    }
+  }
+  const result = await userCollection.updateOne(filter, updatedDoc);
+  res.send(result);
+})
 
 //  carts collection
 
